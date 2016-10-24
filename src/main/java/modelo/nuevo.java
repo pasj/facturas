b76fd9a3fe8116865c828c09e2ae471163ac41c4
Pablo Sevilla <pasj@guegue.com.ni>
2016-10-24 14:15:07 -0600

    Proyecto Inicial

---
+++ src/main/java/modelo/nuevo.java
@@ -0,0 +1,7 @@
+package modelo;
+
+/**
+ * Created by Pablo Sevilla on 24/10/2016.
+ */
+public class nuevo {
+}
---
+++ src/main/java/utils/ViewScope.java
@@ -0,0 +1,45 @@
+package utils;
+
+import java.util.Map;
+import javax.faces.context.FacesContext;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.config.Scope;
+
+/**
+ * Created by Pablo Sevilla on 24/10/2016.
+ */
+public class ViewScope implements Scope {
+    public ViewScope() {
+    }
+
+    public Object get(String name, ObjectFactory<?> objectFactory) {
+        if(FacesContext.getCurrentInstance().getViewRoot() != null) {
+            Map viewMap = FacesContext.getCurrentInstance().getViewRoot().getViewMap();
+            if(viewMap.containsKey(name)) {
+                return viewMap.get(name);
+            } else {
+                Object object = objectFactory.getObject();
+                viewMap.put(name, object);
+                return object;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    public Object remove(String name) {
+        return FacesContext.getCurrentInstance().getViewRoot() != null?FacesContext.getCurrentInstance().getViewRoot().getViewMap().remove(name):null;
+    }
+
+    public void registerDestructionCallback(String name, Runnable callback) {
+    }
+
+    public Object resolveContextualObject(String key) {
+        return null;
+    }
+
+    public String getConversationId() {
+        return null;
+    }
+}
+
